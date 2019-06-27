da8a81ac1af86e6a8859f36b9440227cf99dfd5c
TACIXAT <TACIXAT@protonmail.com>
2019-06-26 23:51:36 -0400

    Remove plugin module.

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# @Author: TACIXAT
-# @Date:   2019-07
-# @Last modified by:   TACIXAT
-# @Last modified time: 2019-07
-
-from .wasm_arch import WASMArch
-from .wasm_view import WASMView
-
-# https://webassembly.github.io/spec/core/binary/modules.html
